353ce77bbc3aab31b649426018ad57758756757a
Yevhen Khreptun <khreptun.e@gmail.com>
2023-04-21 14:17:55 -0300

    Add css styles

--- Project 2 - Recipe app/js/app.js
+++ Project 2 - Recipe app/js/app.js
@@ -10,7 +10,8 @@ function searchRecipe() {
   const inputValue = recipeInput.value.trim();
 
   if (!inputValue) {
-    recipeContainer.innerHTML = "<h3>You should enter the dish!</h3>";
+    recipeContainer.innerHTML =
+      "<h3 class='attention'>You should enter the dish!</h3>";
     return;
   }
 
@@ -19,28 +20,36 @@ function searchRecipe() {
     .then((data) => {
       if (!data.meals) {
         recipeContainer.innerHTML =
-          "<h3>Sorry, we don't have this dish. Try again!</h3>";
+          "<h3 class='attention'>Sorry, we don't have this dish. Try again!</h3>";
         return;
       }
 
       const meal = data.meals[0];
 
       const recipeHtml = `
-        <div class="recipe-details">
-          <h2>${meal.strMeal}</h2>
-          <h4>${meal.strArea}</h4>
+        <div class="recipe-info">
+          <h2 class="recipe-title">${meal.strMeal}</h2>
+          <h4 class="recipe-area">
+            ${meal.strArea}
+            <span class="material-symbols-outlined">
+              schedule
+            </span>
+            <span>
+            ${getPrepareTime(getItem)}</span>
+          </h4>
         </div>
         <img src=${meal.strMealThumb}>
+        <div class="recipe-buttons">
+          <button class="hide active">Ingredients<button>
+          <button class="show">Instruction</button></div>
         <div class="recipe-ingredients">
-          <h2>Ingredients:</h2>
-          <ul>${buildIngredients(getItem)}</ul>
-          <button id="show">Show instruction<button>
+          <h4 class="ingredients-title">Ingredients:</h4>
+          <ul class="ingredients-list">${buildIngredients(getItem)}</ul>
         </div>
       `;
       const instructionHtml = `
         <div class="recipe-instruction">
           <p>${meal.strInstructions}</p>
-          <button id="hide">Return</button>
         </div>
       `;
 
@@ -60,14 +69,39 @@ function searchRecipe() {
         return (recipeContainer.innerHTML = ingredientsHtml);
       }
 
-      const showBtn = document.querySelector("#show");
+      function getPrepareTime(getIngredients) {
+        const ingredients = getIngredients(meal, "strIngredient");
+        let time = 0;
+        if (getIngredients.length > 0) {
+          for (const ingredient of ingredients) {
+            time += 5;
+          }
+        }
+
+        if (time === 60) {
+          return `0:${time}`;
+        }
+
+        if (time > 60) {
+          let hours = Math.trunc(time / 60);
+          let minutes = time % 60;
+          if (minutes < 60) {
+            minutes = `0${minutes}`;
+          }
+          return `${hours}:${minutes}`;
+        }
+
+        return `0:${time}`;
+      }
+
+      const showBtn = document.querySelector(".show");
       showBtn.addEventListener("click", showInstruction);
 
-      const hideBtn = document.querySelector("#hide");
+      const hideBtn = document.querySelector(".hide");
       hideBtn.addEventListener("click", hideInstruction);
     })
     .catch(() => {
-      recipeContainer.innerHTML = `<h3>Sorry, the server doesn't available now. Try late!</h3>`;
+      recipeContainer.innerHTML = `<h3 class='attention'>Sorry, the server doesn't available now. Try late!</h3>`;
     });
 }
 
@@ -84,12 +118,18 @@ function getItem(mealObject, itemName) {
   return getItemArray;
 }
 
-function showInstruction() {
-  document.querySelector(".recipe-instruction").style.display = "block";
-  document.querySelector(".recipe-ingredients").style.display = "none";
+function showInstruction(event) {
+  const recipeInstruction = document.querySelector(".recipe-instruction");
+  const recipeIngredients = document.querySelector(".recipe-ingredients");
+
+  recipeInstruction.style.display = "block";
+  recipeIngredients.style.display = "none";
 }
 
-function hideInstruction() {
-  document.querySelector(".recipe-instruction").style.display = "none";
-  document.querySelector(".recipe-ingredients").style.display = "block";
+function hideInstruction(event) {
+  const recipeInstruction = document.querySelector(".recipe-instruction");
+  const recipeIngredients = document.querySelector(".recipe-ingredients");
+
+  recipeInstruction.style.display = "none";
+  recipeIngredients.style.display = "block";
 }
